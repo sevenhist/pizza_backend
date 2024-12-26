6bda4ae38edaaf3327d542aa1b0bd953fbc24d75
Rudnenko Dmytro <sevenhist@gmail.com>
2024-12-26 14:23:40 +0100

    app.ts update

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL
+    origin: process.env.FRONTEND_URL || '*',
 }));
 app.use('/api', router);
 app.use(errorMiddleware);
