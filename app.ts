5ce08a45ddaa38dd6178e3bd9090537f99037cb7
Rudnenko Dmytro <sevenhist@gmail.com>
2025-01-17 13:22:24 +0100

    app.ts update

--- app.ts
+++ app.ts
@@ -3,19 +3,20 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 import errorMiddleware from './middlewares/error-middleware.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT: number = Number(process.env.PORT) || 3002; // Typisierung für PORT
 const API_URL: string = process.env.API_URL || 'http://localhost:3002'; // API_URL mit Fallback
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors(
-//     {
-//     origin: "https://sevenhist.github.io/pizzaiolo",
-// }
-));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api', router);
 app.use(errorMiddleware);
 
